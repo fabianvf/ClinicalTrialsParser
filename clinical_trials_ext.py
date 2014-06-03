39d6c0480260e415964b810c98af8e8010fe7847
Peter Fan <peter.j.fan@gmail.com>
2014-06-03 16:03:39 -0400

    Modified ClinicalTrialData (CTD) class to accept multiple contributors for a study. Added a function to CTD class that returns json in format that OSF will accept.

--- clinical_trials_ext.py
+++ clinical_trials_ext.py
@@ -10,15 +10,32 @@ class ClinicalTrialData(object):
 		self.json = self.request.json()
 		self.title = self.json['results'][0]['official_title']
 		self.description = self.json['results'][0]['brief_summary']['textblock']
-		if self.has_first_name():
-			self.author_name = self.json['results'][0]['overall_official'][0]['first_name'] + self.json['results'][0]['overall_official'][0]['last_name']
-		else:
-			self.author_name = self.json['results'][0]['overall_official'][0]['last_name']
+		self.contributors = self.__populate_contributors()
 	def json_to_txt(self):
 		with open((self.id + '.json'), 'w') as json_txt:
 			json.dump(self.json, json_txt, sort_keys=True, indent=4)
-	def has_first_name(self):
-		if self.json['results'][0]['overall_official'][0]['first_name'] == None:
-			return False
-		else:
-			return True
+	def __populate_contributors(self):
+		contributors = []
+		for entry in self.json['results'][0]['overall_official']:
+			contr_dict = {}	
+			if entry['first_name'] == None:
+				contr_dict['full_name'] = entry['last_name']
+			else:
+				contr_dict['full_name'] = entry['first_name'] + entry['last_name']
+			contr_dict['role'] = entry['role']
+			contributors.append(contr_dict)
+		return contributors
+	def json_osf_format(self):
+		json_osf = {
+    		"source": "lilly",
+    		"tags": [
+        		"imported",
+        		"clinical_trial"
+    		]
+		}
+		json_osf['contributors'] = self.contributors
+		json_osf['title'] = self.title
+		json_osf['description'] = self.description
+		json_osf['id'] = self.id
+		json_osf['url'] = 'http://clinicaltrials.gov/ct2/show/' + self.id
+		return json_osf
