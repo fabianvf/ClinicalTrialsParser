39ae7b5f5b4cb8b12c16890dc233765778fc9e91
erinbspace <erin.braswell@gmail.com>
2014-06-04 15:07:27 -0400

    updated ids to also include date last modified

--- get_all_ids.py
+++ get_all_ids.py
@@ -9,7 +9,7 @@ def get_all_ids():
 	limit = 10000
 	offset = 0
 
-	# get the total number of ids in the API
+	get the total number of ids in the API
 	url = 'http://api.lillycoi.com/v1/trials.json?fields=totalCount'
 	initial = requests.get(url)
 	initial_json = initial.json()
@@ -17,16 +17,20 @@ def get_all_ids():
 	time.sleep(1)
 
 	id_list = []
+	inner_list = []
 
-	while offset < total:
+	while offset <= total:
+
 		partial_url = 'http://api.lillycoi.com/v1/trials.json?limit=' + str(limit) + '&fields=id,lastchanged_date&offset=' + str(offset)
 		partial_ids = requests.get(partial_url)
 		partial_ids_json = partial_ids.json()
-		
 		partial_ids = partial_ids_json['results']
 
 		for item in partial_ids:
-			id_list.append(item.get('id'))
+			inner_list.append(item.get('id'))
+			inner_list.append(item.get('lastchanged_date')['value'])
+			id_list.append(inner_list)
+			inner_list = []
 
 		print "got " + str(offset) + " trials!"
 
@@ -35,7 +39,7 @@ def get_all_ids():
 		time.sleep(3)
 
 	# save that ID list to a file
-	file_name = open('all_ids.txt', 'w')
+	file_name = open('all_ids2.txt', 'w')
 	for item in id_list:
 		file_name.write("%s\n" % item)
 
