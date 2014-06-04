191d5f5de9bbdaf715dc1572d7a7f6a6dd6942e8
Michelle <xy4kj@virginia.edu>
2014-06-04 11:52:58 -0400

    bulk_trials takes in list of ids

--- bulk_trials.py
+++ bulk_trials.py
@@ -2,14 +2,11 @@ import requests
 import json
 from clinical_trials_ext import ClinicalTrialData
 class bulk_trials(object):
-	def __init__(self, offset, limit):
-		self.offset=offset
-		self.limit=limit
-		self.url="http://api.lillycoi.com/v1/trials.json?offset=" + str(offset) + "&limit=" + str(limit)
-		self.req=requests.get(self.url)
-		self.json=self.req.json()
-		self.id = None
-	
+	def __init__(self, id_list):
+		self.id_list=id_list
+		self.url = None
+		self.req = None
+		self.json = None
 
 	def create_CT_list(self):
 
@@ -17,16 +14,21 @@ class bulk_trials(object):
 
 		ct_list=[]
 
-		while i < self.limit:
-			ct =  ClinicalTrialData(self.json['results'][i]['id'])
+		while i < len(self.id_list):
+			self.url="http://api.lillycoi.com/v1/trials/"+ self.id_list[i] + ".json"
+			self.req=requests.get(self.url)
+			self.json=self.req.json()
+			ct =  ClinicalTrialData(self.id_list[i])
 			ct_list.append(ct)
 			i+=1
 
 		return ct_list
 
 			
-
-q = bulk_trials(4, 100)
-ll = q.create_CT_list()
-print len(ll)
+#a = ["NCT00552448", "NCT00000116"]
+#q = bulk_trials(a)
+#ll = q.create_CT_list()
+#print len(ll)
+#print ll[0].description
+#print ll[1].description
 
