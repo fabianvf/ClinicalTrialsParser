2ab40e6a76471108ff24bbc7bb653baeba7bb15f
Peter Fan <peter.j.fan@gmail.com>
2014-06-06 14:25:52 -0400

    Updated ClinicalTrialData class so that geo_data and references were also extracted from the raw XML.

--- clinical_trials_parser.py
+++ clinical_trials_parser.py
@@ -1,13 +1,8 @@
 import requests
 import json
-import time
-import datetime
 from lxml import etree
 
-#TODO: Agree on a naming system for json/xml files, and the json for OSF import?
-#TODO: Do we need to change or remove the time-stamp? It's generated when the instance of the object is created which is no longer the same time as when the data is retrieved from the server.
 #TODO: Write test code.
-#TODO: Extract location data and citation/reference data.
 
 class ClinicalTrialData(object):
 	"""Parses an XML file containing clinical trial study data from clinicaltrials.gov"""
@@ -32,23 +27,40 @@ class ClinicalTrialData(object):
 			self.keywords.append(entry.text)
 		self.current_status = self.root.find('overall_status').text
 		self.phase = self.root.find('phase').text
-		self.time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-
+		self.date_processed = self.__process_date()
+		self.references = []
+		for entry in self.root.findall('reference'):
+			reference_dict = {}
+			reference_dict['citation'] = entry.find('citation').text
+			reference_dict['PMID'] = entry.find('PMID').text
+			self.references.append(reference_dict)
+		self.locations = []
+		for entry in self.root.findall('location'):
+			location_tuple = ((entry.find('facility').find('name').text), (entry.find('facility').find('address').find('zip').text))
+			self.locations.append(location_tuple)
+	def __process_date(self):
+		date_string = self.root.find('required_header').find('download_date').text
+		date_string = date_string.replace('ClinicalTrials.gov processed this data on ', '')
+		return date_string
 	def json_osf_format(self):
 		"""Returns a dictionary that represents key information about the clinical trial in json format."""
 		json_osf = {
-    		"source": "clinicaltrials.gov",
-    		"time_retrieved": self.time_stamp,
+    		"imported_from": "clinicaltrials.gov",
+    		"date_processed": self.date_processed,
     		"contributors": None,
     		"description": None,
     		"id": self.id,
     		"title": None,
     		"url": None,
+    		"files": str(self.id) + '.xml',
     		"tags": [
         		"clinical trial"
     		],
     		"phase": None,
     		"current_status": None,
+    		"geo_data": None,
+    		"references": None,
+    		"components": [{"title":"MetaData","description":None}]
 		}
 		json_osf['contributors'] = self.contributors
 		json_osf['title'] = self.title
@@ -58,9 +70,13 @@ class ClinicalTrialData(object):
 		json_osf['tags'] = self.keywords
 		json_osf['phase'] = self.phase
 		json_osf['current_status'] = self.current_status
+		json_osf['geo_data'] = self.locations
+		json_osf['references'] = self.references
 		return json_osf
 
 	def json_osf_to_txt(self):
-		with open(('CTgov_study_' + str(self.id) + '.json'), 'w') as json_txt:
+		with open((str(self.id) + '_osf.json'), 'w') as json_txt:
 			json.dump(self.json_osf_format(), json_txt, sort_keys=True, indent=4)
 
+x = ClinicalTrialData('NCT00001372.xml')
+print x.json_osf_to_txt()
