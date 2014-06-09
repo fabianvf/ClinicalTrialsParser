924eccf26103162b3e64111e2ef9162f57e9a443
erinbspace <erin.braswell@gmail.com>
2014-06-09 17:17:22 -0400

    can get the raw XML from a changes page

--- ct_archive/archive_html_to_xml.py
+++ ct_archive/archive_html_to_xml.py
@@ -8,9 +8,10 @@ tr class = "sdiff-unc" or class = "sdiff-add" or class = "sdiff-chg"
 ''' 
 
 from bs4 import BeautifulSoup
+import re
 
 # testing on the first change file for NCT00000122
-soup = BeautifulSoup(open("./ct_archive/ct_changes/NCT00000122/2005_06_30.html")) 
+soup = BeautifulSoup(open("./ct_changes/NCT00000122/2005_06_30.html")) 
 
 # this is a bs4 Tag object of just the sdiff-full stuff
 sdiff_full = soup.find(id="sdiff-full")
@@ -25,15 +26,20 @@ all_before = []
 for result in sdiff_xml:
     all_before.append(result.find("td", {"class" : "sdiff-a"}))
 
+
+before_file = open('before.xml', 'w')
+
+for item in all_before:
+    before_file.write("%s\n" % item.text)
+
+
 # td class sdiff-b: all of the "after" xml
 all_after = []
 
 for result in sdiff_xml:
     all_after.append(result.find("td", {"class" : "sdiff-b"}))
 
-# opening tags have class="sdl(some#) sds"
-# closing tags have class="sdl(same#) sdz"
-# text inside those tags has class="sdl(same#)"
+after_file = open('after.xml', 'w')
 
-# use regex to find all classes with sdl in the name... within the 
-# "sdiff-full" thing... 
+for item in all_after:
+    after_file.write("%s\n" % item.text)
