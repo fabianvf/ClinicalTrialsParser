a8cfdc9854b6fff9c2aff267d6fee6c3327f90cd
erinbspace <erin.braswell@gmail.com>
2014-06-12 10:33:31 -0400

    added spaces instead of tabs to clinical_trials_parser, added some try except blocks, added 8000 raw json files

--- clinical_trials_parser.py
+++ clinical_trials_parser.py
@@ -7,87 +7,90 @@ from lxml import etree
 
 
 class ClinicalTrialData(object):
-	"""Parses an XML file containing clinical trial study data from clinicaltrials.gov"""
-	def __init__(self,file_name):
-		"""Upon init, reads XML file,and parses it for some key information.
-		NOTE: Comment tags are included in the XML tree."""
-		self.file_name = str(file_name)
-		self.root = etree.parse(self.file_name).getroot()
-		self.id = self.root.find('id_info').find('nct_id').text
-		self.url = self.root.find('required_header').find('url').text
-		self.current_status = self.root.find('overall_status').text
-		self.phase = self.root.find('phase').text
-		self.description = self.root.find('brief_summary')[0].text
+    """Parses an XML file containing clinical trial study data from clinicaltrials.gov"""
+    def __init__(self,file_name):
+        """Upon init, reads XML file,and parses it for some key information.
+        NOTE: Comment tags are included in the XML tree."""
+        self.file_name = str(file_name)
+        self.root = etree.parse(self.file_name).getroot()
+        self.id = self.root.find('id_info').find('nct_id').text
+        self.url = self.root.find('required_header').find('url').text
+        self.current_status = self.root.find('overall_status').text
+        self.phase = self.root.find('phase').text
+        self.description = self.root.find('brief_summary')[0].text
 
-		if self.root.find('official_title') == None:
-			self.title = self.root.find('brief_title').text
-		else:
-			self.title = self.root.find('official_title').text
+        if self.root.find('official_title') == None:
+            self.title = self.root.find('brief_title').text
+        else:
+            self.title = self.root.find('official_title').text
 
-		self.contributors = []
-		for entry in self.root.findall('overall_official'):
-			official_dict = {}
-			official_dict['full_name'] = entry.find('last_name').text
-			official_dict['role'] = entry.find('role').text
-			official_dict['affiliation'] = entry.find('affiliation').text
-			official_dict['email'] = None
-			self.contributors.append(official_dict)
-		
-		self.keywords = ["clinical trial"]
-		for entry in self.root.findall('keyword'):
-			self.keywords.append(entry.text)
-		self.date_processed = self.__process_date()
+        self.contributors = []
+        for entry in self.root.findall('overall_official'):
+            try:
+                official_dict = {}
+                official_dict['full_name'] = entry.find('last_name').text
+                official_dict['role'] = entry.find('role').text
+                official_dict['affiliation'] = entry.find('affiliation').text
+                official_dict['email'] = None
+            except AttributeError:
+                pass
+            self.contributors.append(official_dict)
+        
+        self.keywords = ["clinical trial"]
+        for entry in self.root.findall('keyword'):
+            self.keywords.append(entry.text)
+        self.date_processed = self.__process_date()
 
-		self.references = []
-		for entry in self.root.findall('reference'):
-			reference_dict = {'PMID':None}
-			reference_dict['citation'] = entry.find('citation').text
-			if entry.find('PMID') != None:
-				reference_dict['PMID'] = entry.find('PMID').text
-			self.references.append(reference_dict)
-			
-		self.locations = []		
-		for entry in self.root.findall('location'):
-			location_dict = {'location_name': None, 'location_zip': None}
-			if entry.find('facility').find('name') != None:
-				location_dict['location_name'] = entry.find('facility').find('name').text
-			if entry.find('facility').find('address').find('zip') != None:
-				location_dict['location_zip'] = entry.find('facility').find('address').find('zip').text
-			self.locations.append(location_dict)
+        self.references = []
+        for entry in self.root.findall('reference'):
+            reference_dict = {'PMID':None}
+            reference_dict['citation'] = entry.find('citation').text
+            if entry.find('PMID') != None:
+                reference_dict['PMID'] = entry.find('PMID').text
+            self.references.append(reference_dict)
+            
+        self.locations = []     
+        for entry in self.root.findall('location'):
+            location_dict = {'location_name': None, 'location_zip': None}
+            if entry.find('facility').find('name') != None:
+                location_dict['location_name'] = entry.find('facility').find('name').text
+            if entry.find('facility').find('address').find('zip') != None:
+                location_dict['location_zip'] = entry.find('facility').find('address').find('zip').text
+            self.locations.append(location_dict)
 
-	def __process_date(self):
-		"""Private method for parsing the xml for the date processed (by clinicaltrials.gov) info."""
-		date_string = self.root.find('required_header').find('download_date').text
-		date_string = date_string.replace('ClinicalTrials.gov processed this data on ', '')
-		return date_string
+    def __process_date(self):
+        """Private method for parsing the xml for the date processed (by clinicaltrials.gov) info."""
+        date_string = self.root.find('required_header').find('download_date').text
+        date_string = date_string.replace('ClinicalTrials.gov processed this data on ', '')
+        return date_string
 
-	def json_osf_format(self):
-		"""Returns a dictionary that represents key information about the clinical trial in json format."""
-		json_osf = {
-    		"imported_from": "clinicaltrials.gov",
-    		"date_processed": self.date_processed,
-    		"contributors": self.contributors,
-    		"description": self.description,
-    		"id": self.id,
-    		"title": self.title,
-    		"url": self.url,
-    		"files": [('../ct_xml/' + str(self.id) + '.xml'), ('../ct_raw_json/' + str(self.id) + '_raw.json' )],
-    		"tags": [
-        		"clinical trial"
-    		],
-    		"phase": self.phase,
-    		"current_status": self.current_status,
-    		"geo_data": self.locations,
-    		"references": self.references,
-    		"components": [{"title":"MetaData","description":None}]
-		}
-		json_osf['tags'] = self.keywords
-		return json_osf
+    def json_osf_format(self):
+        """Returns a dictionary that represents key information about the clinical trial in json format."""
+        json_osf = {
+            "imported_from": "clinicaltrials.gov",
+            "date_processed": self.date_processed,
+            "contributors": self.contributors,
+            "description": self.description,
+            "id": self.id,
+            "title": self.title,
+            "url": self.url,
+            "files": [('../ct_xml/' + str(self.id) + '.xml'), ('../ct_raw_json/' + str(self.id) + '_raw.json' )],
+            "tags": [
+                "clinical trial"
+            ],
+            "phase": self.phase,
+            "current_status": self.current_status,
+            "geo_data": self.locations,
+            "references": self.references,
+            "components": [{"title":"MetaData","description":None}]
+        }
+        json_osf['tags'] = self.keywords
+        return json_osf
 
-	def json_osf_to_txt(self):
-		"""Writes the json returned by json_osf_format() to a text file."""
-		with open((str(self.id) + '_osf.json'), 'w') as json_txt:
-			json.dump(self.json_osf_format(), json_txt, sort_keys=True, indent=4)
+    def json_osf_to_txt(self):
+        """Writes the json returned by json_osf_format() to a text file."""
+        with open((str(self.id) + '_osf.json'), 'w') as json_txt:
+            json.dump(self.json_osf_format(), json_txt, sort_keys=True, indent=4)
 
 
 
--- xml_to_json.py
+++ xml_to_json.py
@@ -6,25 +6,25 @@ import xmltodict
 import glob
 from open_map_gps import get_coordinate
 from clinical_trials_parser import ClinicalTrialData
-def xml_to_json(xml_file):
+def xml_to_json(xml_file, nct_id):
 
     tree = etree.parse(xml_file)
     ctd = ClinicalTrialData(xml_file)
     loc_list = ctd.locations
     xml_string = etree.tostring(tree)
-    nct_id = xml_file[0:len(xml_file)-4]
 
     json_text = xmltodict.parse(xml_string)
 
     json_text['geocoordinates'] = get_coordinate(nct_id)
+    
     json_text = json.dumps(json_text, sort_keys=True, indent=4)
     
     return json_text
 
 def key_files_to_json():
 
-    key_xml_folder = 'ct_xml/'
-    key_json_folder = 'ct_raw_json/'
+    key_xml_folder = 'all_studies/'
+    key_json_folder = 'all_raw_json/'
 
     xmlfiles = glob.glob(key_xml_folder + '*.xml')
 
@@ -32,18 +32,18 @@ def key_files_to_json():
     xml_ids = [ext.replace('.xml','') for ext in xmlfiles]
 
     # get rid of the folder name
-    xml_ids = [folder.replace('ct_xml/','') for folder in xml_ids]
+    xml_ids = [folder.replace(key_xml_folder,'') for folder in xml_ids]
 
     id_index = 0
 
     for filename in xmlfiles:
-        json_text = xml_to_json(filename)
+        json_text = xml_to_json(filename, xml_ids[id_index])
 
         with open(key_json_folder + xml_ids[id_index] +'_raw.json', "wb") as jt:
             jt.write(json_text)
 
         id_index += 1
 
-print xml_to_json('NCT00604695.xml')
+# xml_to_json('ct_xml/NCT00000122.xml')
 
-#key_files_to_json()
+key_files_to_json()
