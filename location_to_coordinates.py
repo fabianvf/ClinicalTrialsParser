a78758cf97002a4a2277f812dd682cce64bb8e1c
Peter Fan <peter.j.fan@gmail.com>
2014-06-12 14:29:29 -0400

    Confirmed the location_to_coordinates.py function works.

--- location_to_coordinates.py
+++ location_to_coordinates.py
@@ -1,7 +1,10 @@
 import requests
 import json
 
-def GetCoordinates(city, country, location=None, zip=None, state=None):
+def GetCoordinates(city, country, zip=None, state=None):
+	"""Takes location data (city, state, zip, country) as input. Sends a GET request to the open street maps/mapquest API and 
+	pulls latitude/longitude information out of the Json that's returned."""
+
 	url = 'http://open.mapquestapi.com/nominatim/v1/search.php'
 	parameters = {'city': city, 'country': country, 'postalcode': None, 'state': state, 'format': 'json' }
 	if country == 'United States':
@@ -10,6 +13,8 @@ def GetCoordinates(city, country, location=None, zip=None, state=None):
 	request_json = request.json()
 	return {'latitude': request_json[0]['lat'], 'longitude': request_json[0]['lon']}
 
-json_cord = GetCoordinates('Baltimore', 'United States', state='Maryland', zip='21224 6823')
+
+json_cord = GetCoordinates('Baltimore', 'United States', state="MD", zip='21224 6823')
 
 print json_cord
+                          
