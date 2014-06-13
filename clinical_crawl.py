64d244800cbe5eec324129bd979c40cf47bad0b0
Peter Fan <peter.j.fan@gmail.com>
2014-06-13 14:52:04 -0400

    Deleted json and xml files used for testing embedded street map, updated ct_osf_json files to most recent format, wrote script to automate converting raw xml files to osf_json format.

--- clinical_crawl.py
+++ clinical_crawl.py
@@ -5,29 +5,29 @@ from bs4 import BeautifulSoup
 a clinicaltrials.gov page for webcrawlers."""
 
 def ClinicalCrawl():
-	data = requests.get("http://clinicaltrials.gov/ct2/about-site/crawling").text
-	crawl_page_soup = BeautifulSoup(data)
+    data = requests.get("http://clinicaltrials.gov/ct2/about-site/crawling").text
+    crawl_page_soup = BeautifulSoup(data)
 
-	trial_links=[]
+    trial_links=[]
 
-	for link in crawl_page_soup.find_all('a'):
-		valid_link = link.get('href')
-		if(valid_link[0:11] == "/ct2/crawl/"):
-			trial_links.append(valid_link)
+    for link in crawl_page_soup.find_all('a'):
+        valid_link = link.get('href')
+        if(valid_link[0:11] == "/ct2/crawl/"):
+            trial_links.append(valid_link)
 
-	trial_id_list=[]
-	id_file=open("nct_id_list.txt", 'w')
+    trial_id_list=[]
+    id_file=open("nct_id_list.txt", 'w')
 
-	for group in trial_links:
-		data = requests.get("http://clinicaltrials.gov"+group).text
-		link_soup = BeautifulSoup(data)
-		for link in link_soup.find_all('a'):
-			valid_link = link.get('href')
-			if(valid_link[0:10] == "/ct2/show/"):
-				nct_id = valid_link[10:]
-				print nct_id
-				id_file.write(repr(nct_id)+"\n")
+    for group in trial_links:
+        data = requests.get("http://clinicaltrials.gov"+group).text
+        link_soup = BeautifulSoup(data)
+        for link in link_soup.find_all('a'):
+            valid_link = link.get('href')
+            if(valid_link[0:10] == "/ct2/show/"):
+                nct_id = valid_link[10:]
+                print nct_id
+                id_file.write(repr(nct_id)+"\n")
 
-	id_file.close()
+    id_file.close()
 
 ClinicalCrawl()
--- example_json.js
+++
@@ -1,9 +0,0 @@
-var geo_data = {
-    city: 'Charlottesville', 
-    country: 'United States', 
-    name: 'Center for Research in Reproduction, University of Virginia', 
-    state: 'Virginia', 
-    zip: '22908'
-    latitude: '38.029306'
-    longitude: '-78.4766781' 
-};
